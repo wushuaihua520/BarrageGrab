4b28647b36eb7e8d39caa4e9061a1fb11822baec
Sandy <wushuaihua@vip.qq.com>
2024-08-06 21:50:05 +0800

    签名服务搬迁

--- BarrageGrab/GlobalConfigs.cs
+++ BarrageGrab/GlobalConfigs.cs
@@ -31,14 +31,13 @@ namespace BarrageGrab
         #region 签名服务
 
         /// <summary>
-        /// 2H2G 2M带宽服务器，2024-06-25后，大家打赏的金额，将用作于服务器。
-        /// 如需独立部署版本，请联系群主。
+        /// 如需独立部署版本的签名服务，请联系群主。
         /// </summary>
-        internal static string SignApi_Domain { get; } = "http://129.204.171.194:8081"; //2H2G 2M 临时服务器
-        internal static string SignApi_Url { get; } = "/Douyin/SignWss";
+        internal static string SignApi_Domain { get; } = "https://api.aiobs.cn";
+        internal static string SignApi_Url { get; } = "/Douyin/Douyin/SignWss";
 
         /// <summary>
-        /// 测试key，每分钟、每个key、每个独立IP，请求有限制，为：每分钟/key/IP 共10 times，且
+        /// 测试key，每分钟、每个key、每个独立IP，请求有限制，例如：每分钟/key/IP 共10 times
         /// 如需申请独立key，请联系群主。
         /// </summary>
         internal static string SignApi_Key { get; } = "test-apikey-de9991ea-bf2b-454c-7982-adddfe0581ac-96c0642e-7d1d-87a7-08b2-eff81edae4d3";
